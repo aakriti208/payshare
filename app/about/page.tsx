d5b09a7373f5dd045ab232c40d9a0cfd487c380f
aakriti208 <aakritidh208@gmail.com>
2024-11-07 08:59:23 -0600

    Database setup

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
-export default function About(){
-    return (
-        <div>
-            About
-        </div>
-    )
+import { getData } from "@/actions/todoAction";
+import Todos from "@/components/todos";
+
+export default async function Home() {
+  const data = await getData();
+  return <Todos todos={data} />;
 }
