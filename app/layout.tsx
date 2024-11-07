2324b586f3db1285ed66dc41d75107197ed0c8bd
aakriti208 <aakritidh208@gmail.com>
2024-11-07 07:57:32 -0600

    Initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
+import About from "./about/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+        <Link href={"/about"}>
+        About
+        </Link>
+          
+        </div>
         {children}
+        Footer
       </body>
     </html>
   );
